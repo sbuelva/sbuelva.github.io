a9aa60845b02b8cb763a66ae0c75576bf4d9355b
sbuelva <126602992+sbuelva@users.noreply.github.com>
2023-02-28 18:09:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+document.getElementById('btn_+info_1').onclick = function () { 
+    console.log("Capturamos el evento click");
+    document.getElementById ('mas_informacion_1').style.display='block';
+    document.getElementById ('btn_-info_1').style.display='block';
+}
+
+document.getElementById('btn_-info_1').onclick = function () { 
+    console.log("Capturamos el evento click");
+    document.getElementById ('mas_informacion_1').style.display='none';
+}
+
+
+
+
+document.getElementById('btn_+info_2').onclick = function () { 
+    console.log("Capturamos el evento click");
+    document.getElementById ('mas_informacion_2').style.display='block';
+    document.getElementById ('btn_-info_2').style.display='block';       
+}
+    
+document.getElementById('btn_-info_2').onclick = function () {  
+    console.log("Capturamos el evento click");
+    document.getElementById ('mas_informacion_2').style.display='none';
+}
+
+
+
+
+document.getElementById('btn_+info_3').onclick = function () { 
+    console.log("Capturamos el evento click");
+    document.getElementById ('mas_informacion_3').style.display='block';
+    document.getElementById ('btn_-info_3').style.display='block';
+}
+
+document.getElementById('btn_-info_3').onclick = function () { 
+    console.log("Capturamos el evento click");
+    document.getElementById ('mas_informacion_3').style.display='none';
+}
